aff9bb5377a4e9cf1c5a1859c46dfb48962791a3
BlackSoulKnight <44965005+BlackSoulKnight@users.noreply.github.com>
2026-06-08 01:23:46 +0200

    histoire, wrong weapon id

--- src/Data/Neptunia 2 Data/ProgressiveGear/HistoireGear.cs
+++ src/Data/Neptunia 2 Data/ProgressiveGear/HistoireGear.cs
@@ -13,7 +13,7 @@ namespace Nep3ArchipelagoClient.Neptunia_2_Data.ProgressiveGear
             GearList.Add(0, new List<short>() { 1381 });
             GearList.Add(1, new List<short>() { 1382 });
             GearList.Add(2, new List<short>() { 1384 });
-            GearList.Add(3, new List<short>() { 1286 });
+            GearList.Add(3, new List<short>() { 1386 });
         }
     }
 }

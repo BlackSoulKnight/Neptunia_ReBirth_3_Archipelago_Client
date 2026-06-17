18fb32a315dfbbd3b715b20362341399ae80819b
BlackSoulKnight <44965005+BlackSoulKnight@users.noreply.github.com>
2026-06-17 16:41:28 +0200

    add event 0 to fix cutscene loop?

--- src/Data/Neptunia 3 Data/Events.cs
+++ src/Data/Neptunia 3 Data/Events.cs
@@ -10,7 +10,7 @@ namespace Nep3ArchipelagoClient.Neptunia_3_Data
     {
         public Events()
         {
-            PermanentEvents.AddRange([1,2,3,4,18,19,20,21,22,23,1014,1015,3014,3013,3332,8992]);
+            PermanentEvents.AddRange([0,1,2,3,4,18,19,20,21,22,23,1014,1015,3014,3013,3332,8992]);
             UnlockableEvents.AddRange([1013]);
         }
     }
